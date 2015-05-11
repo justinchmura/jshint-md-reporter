a665215203e1b794e57e32ae804cfb08fe3c9ded
Justin Chmura <justin.chmura@gmail.com>
2015-05-11 10:22:30 -0700

    Setup check for valid errors and create tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,39 @@
+var R = require('ramda');
 var assert = require('assert');
+var reporter = require('../lib/reporter');
+
+var defaultMsg = 'JSHint found no errors.';
+var testReporter = R.pipe(R.flip, R.curry)(reporter.reporter)(true);
 
 describe('jshint-md-reporter', function () {
 
-  it('runs tests', function () {
-    assert.equal(2, 1+1);
+  describe('No Errors', function () {
+
+    it('returns default message on undefined', function () {
+      var result = testReporter(undefined);
+      assert.equal(result, defaultMsg);
+    });
+
+    it('returns default message on null', function () {
+      var result = testReporter(null);
+      assert.equal(result, defaultMsg);
+    });
+
+    it('returns default message on empty string', function () {
+      var result = testReporter('');
+      assert.equal(result, defaultMsg);
+    });
+
+    it('returns default message on empty array', function () {
+      var result = testReporter([]);
+      assert.equal(result, defaultMsg);
+    });
+
+    it('returns default message on empty object', function () {
+      var result = testReporter({});
+      assert.equal(result, defaultMsg);
+    });
+
   });
 
 });
