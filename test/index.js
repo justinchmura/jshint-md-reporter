3ffad733c27e99d43318567cfbe53a6f7be920d7
Justin Chmura <justin.chmura@gmail.com>
2015-10-20 08:29:53 -0700

    Fix unit tests

--- test/index.js
+++ test/index.js
@@ -3,7 +3,8 @@ const R = require('ramda');
 const assert = require('assert');
 const reporter = require('../lib/reporter');
 
-const defaultMsg = fs.readFileSync('./lib/templates/noItems.md');
+const removeNewLines = R.replace(/^\s+|\s+$/g, '');
+const defaultMsg = removeNewLines(fs.readFileSync('./lib/templates/noItems.md').toString());
 const testReporter = R.pipe(R.flip, R.curry)(reporter.reporter)(true);
 
 describe('jshint-md-reporter', function () {
@@ -11,27 +12,27 @@ describe('jshint-md-reporter', function () {
   describe('No Errors', function () {
 
     it('returns default message on undefined', function () {
-      var result = testReporter(undefined);
+      const result = testReporter(undefined);
       assert.equal(result, defaultMsg);
     });
 
     it('returns default message on null', function () {
-      var result = testReporter(null);
+      const result = testReporter(null);
       assert.equal(result, defaultMsg);
     });
 
     it('returns default message on empty string', function () {
-      var result = testReporter('');
+      const result = testReporter('');
       assert.equal(result, defaultMsg);
     });
 
     it('returns default message on empty array', function () {
-      var result = testReporter([]);
+      const result = testReporter([]);
       assert.equal(result, defaultMsg);
     });
 
     it('returns default message on empty object', function () {
-      var result = testReporter({});
+      const result = testReporter({});
       assert.equal(result, defaultMsg);
     });
 
